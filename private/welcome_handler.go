9f758fcfcd26cdee9f080c2df535aca9fc836f98
Kane Rogers <kane.rogers@bluereef.com.au>
2015-05-07 13:28:42 +1000

    Implemented tests/mocks for table interface.

--- private/welcome_handler.go
+++ private/welcome_handler.go
@@ -2,7 +2,6 @@ package private
 
 import (
 	"fmt"
-	"github.com/AdRoll/goamz/aws"
 	"github.com/AdRoll/goamz/dynamodb"
 	"github.com/kanerogers/test_app/common"
 )
@@ -16,23 +15,11 @@ type User struct {
 	LastName  string `json:"last_name"`
 }
 
-func (*WelcomeHandler) GetStatus(name string) test_app.Status {
-	// AWS Authentication.
-	auth := aws.Auth{AccessKey: "AKIAIBL23CYNOCMPOJ7Q", SecretKey: "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"}
-	region := aws.GetRegion("ap-southeast-2")
-
-	// Setup a server.
-	server := dynamodb.New(auth, region)
-
-	// Setup table.
-	primary_key_attribute := &dynamodb.Attribute{Type: "S", Name: "user_id"}
-	primary_key := dynamodb.PrimaryKey{KeyAttribute: primary_key_attribute}
-	table := dynamodb.Table{Server: server, Name: "sproutli_users", Key: primary_key}
-
+func (*WelcomeHandler) GetStatus(name string, user_table Table) test_app.Status {
 	// Get user
 	user_key := &dynamodb.Key{HashKey: "abc123"}
 	user := new(User)
-	err := table.GetDocument(user_key, user)
+	err := user_table.GetDocument(user_key, user)
 
 	if err != nil {
 		fmt.Printf("Oh shit, there was an error: %s", err)
