1c8b88a53c9ac5fbce19a6f0817180e23dbb4c83
Kane Rogers <kane.m.rogers@gmail.com>
2015-05-10 14:32:41 +1000

    Implement remaining CRUD operations.
    - Refactored test suite.
    - Cleaned code.
    - Implemented CREATE, UPDATE, DELETE.

--- dynamo_crud/dynamo_crud.go
+++ dynamo_crud/dynamo_crud.go
@@ -1,19 +1,21 @@
 package dynamo_crud
 
 import (
+	"code.google.com/p/go-uuid/uuid"
 	"fmt"
 	"github.com/AdRoll/goamz/dynamodb"
 	"github.com/gin-gonic/gin"
-	"github.com/kanerogers/hell_yeah_go/private"
+	"github.com/kanerogers/hell_yeah_go/common"
 	"net/http"
+	"reflect"
 )
 
 type DynamoCRUD struct {
-	private.Table
+	common.Table
 	Resource interface{}
 }
 
-func NewDynomoCRUD(table private.Table, resourceName string, data interface{}) DynamoCRUD {
+func NewDynomoCRUD(table common.Table, resourceName string, data interface{}) DynamoCRUD {
 	return DynamoCRUD{
 		table,
 		data,
@@ -21,13 +23,29 @@ func NewDynomoCRUD(table private.Table, resourceName string, data interface{}) D
 }
 
 func (dynamo *DynamoCRUD) CreateHandler(c *gin.Context) {
-	fmt.Println("Creating")
-	c.JSON(http.StatusOK, gin.H{"status": "create"})
+	// Marshal the POST from JSON to Struct
+	c.Bind(dynamo.Resource)
+
+	// Generate a random ID
+	randomId := uuid.New()
+	key := &dynamodb.Key{HashKey: randomId}
+
+	// Set the ID on the Struct using reflection as we don't know the type here.)
+	reflect.ValueOf(dynamo.Resource).Elem().FieldByName("ID").SetString(randomId)
+
+	// Post the document to DynamoDB
+	err := dynamo.Table.PutDocument(key, dynamo.Resource)
+
+	if err != nil {
+		c.Fail(500, err)
+		return
+	}
+
+	c.JSON(201, dynamo.Resource)
 }
 
 func (dynamo *DynamoCRUD) TakeHandler(c *gin.Context) {
 	resourceName := c.Params[0].Value
-	fmt.Printf("Taking %s\n", resourceName)
 	key := &dynamodb.Key{HashKey: resourceName}
 
 	err := dynamo.Table.GetDocument(key, dynamo.Resource)
@@ -37,7 +55,38 @@ func (dynamo *DynamoCRUD) TakeHandler(c *gin.Context) {
 		return
 	}
 
-	fmt.Printf("Found %s\n", dynamo.Resource)
+	c.JSON(http.StatusOK, dynamo.Resource)
+}
+
+func (dynamo *DynamoCRUD) UpdateHandler(c *gin.Context) {
+	// Marshal the PUT from JSON to Struct
+	c.Bind(dynamo.Resource)
+
+	fmt.Printf("Received %s via JSON PUT\n", c.Request.Body)
+
+	resourceName := c.Params[0].Value
+	key := &dynamodb.Key{HashKey: resourceName}
+
+	err := dynamo.Table.PutDocument(key, dynamo.Resource)
+
+	if err != nil {
+		c.Fail(500, err)
+		return
+	}
 
 	c.JSON(http.StatusOK, dynamo.Resource)
+}
+
+func (dynamo *DynamoCRUD) DeleteHandler(c *gin.Context) {
+	resourceName := c.Params[0].Value
+	key := &dynamodb.Key{HashKey: resourceName}
+
+	err := dynamo.Table.DeleteDocument(key)
+
+	if err != nil {
+		c.Fail(500, err)
+		return
+	}
+
+	c.JSON(204, nil)
 }
--- routing/route_handler_test.go
+++ routing/route_handler_test.go
@@ -1,74 +1,95 @@
 package routing
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/AdRoll/goamz/dynamodb"
-	"github.com/kanerogers/hell_yeah_go/private"
+	"github.com/kanerogers/hell_yeah_go/common"
 	checking "github.com/kanerogers/hell_yeah_go/testing"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	"net/http"
 	"reflect"
 	"testing"
 )
 
 type MockTableMaker struct {
-	mock.Mock
 }
 
-func (m *MockTableMaker) GetTable(resourceName string) private.Table {
-
+func (m *MockTableMaker) GetTable(resourceName string) common.Table {
 	table := new(MockTable)
-	m.Called(resourceName)
-
 	return table
 }
 
 type MockTable struct {
-	mock.Mock
 }
 
 func (m *MockTable) GetDocument(key *dynamodb.Key, v interface{}) error {
-	user := reflect.ValueOf(v).Elem()
-
-	user.Field(0).SetString("abc123")
-	user.Field(1).SetString("Bede")
-	user.Field(2).SetString("Overend")
-
-	args := m.Called(key, v)
-	return args.Error(0)
+	resourceName := key.HashKey
+	resource := reflect.ValueOf(v).Elem()
+	resource.FieldByName("ID").SetString(resourceName)
+	return nil
 }
 
-type Test struct {
-	Status string `json:"status"`
+func (m *MockTable) PutDocument(key *dynamodb.Key, v interface{}) error {
+	resource := reflect.ValueOf(v).Elem()
+	resource.FieldByName("ID").SetString("testing")
+
+	return nil
 }
 
-func TestTakeListing(t *testing.T) {
+func (m *MockTable) DeleteDocument(key *dynamodb.Key) error {
+	return nil
+}
 
-	testPath := "/api/listing/1"
-	testMethod := "GET"
+type TestRoute struct {
+	Method       string
+	ExpectedCode int
+	Path         string
+	Data         interface{}
+}
 
-	expectedCode := http.StatusOK
-	expectedBody := &Test{Status: "take"}
+func TestRoutes(t *testing.T) {
+	testRoutes := []TestRoute{
+		{"GET", 200, "/api/listing/1", &common.Listing{ID: "1"}},
+		{"POST", 201, "/api/listing", &common.Listing{ID: "testing"}},
+		{"PUT", 200, "/api/listing/testing", &common.Listing{ID: "testing", Name: "Testing"}},
+		{"DELETE", 204, "/api/listing/1", nil},
+	}
 
-	// Charset is because gin is funky like that
-	expectedContentType := "application/json; charset=utf-8"
+	for _, testRoute := range testRoutes {
+		actualCode, actualBody := testListing(testRoute.Method, testRoute.Path, testRoute.Data)
 
+		assert.Equal(t, testRoute.ExpectedCode, actualCode)
+		assert.Equal(t, testRoute.Data, actualBody)
+	}
+
+}
+
+func testListing(testMethod string, testPath string, data interface{}) (responseCode int, response interface{}) {
 	tableMaker := new(MockTableMaker)
 	core := Core(tableMaker)
 
-	r := checking.PerformRequest(core, testMethod, testPath)
+	testJSON, _ := json.Marshal(data)
+	testJSONBuffer := bytes.NewBuffer(testJSON)
+
+	fmt.Printf("About to send %s as our buffer\n", testJSONBuffer)
+
+	r := checking.PerformRequest(core, testMethod, testPath, testJSONBuffer)
 
 	actualCode := r.Code
-	actualContentType := r.Header().Get("Content-Type")
 
-	actualBody := new(Test)
+	// If it's a DELETE, we want to send back an empty body, so our "response" is "nil"
+	if testMethod == "DELETE" {
+		return actualCode, nil
+	}
+
+	// In all other cases, decode the Body as JSON into a Struct, then return it.
+	actualBody := new(common.Listing)
 	if err := json.Unmarshal(r.Body.Bytes(), actualBody); err != nil {
+		fmt.Printf("Error unmarshaling JSON for some reason: %e\n", err)
 		panic(err)
 	}
 
-	assert.Equal(t, expectedCode, actualCode)
-	assert.Equal(t, expectedContentType, actualContentType)
-	assert.Equal(t, expectedBody, actualBody)
+	return actualCode, actualBody
 
 }
